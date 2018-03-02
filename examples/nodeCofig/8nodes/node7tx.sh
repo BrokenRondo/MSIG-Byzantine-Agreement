2429043d9f9083ba19a8af693715c63984766c92
walker088 <hanxu2121@gmail.com>
2018-03-02 14:55:34 +0800

    modify shell

--- examples/nodeCofig/8nodes/node7tx.sh
+++ examples/nodeCofig/8nodes/node7tx.sh
@@ -4,7 +4,7 @@ start=$(($(date +%s%N)/1000000))
 for ((i=0;i<1000;i=i+1))
 do
 	(./txs/tx7.sh &)
-	sleep 0.3;
+	sleep 1.1;
 done
 endCli=$(($(date +%s%N)/1000000))
 echo "sending tx from client cost: $(($endCli-$start)) ms"
